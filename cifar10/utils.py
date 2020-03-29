b1bce143224999aa9303676ecb5bfa74c808d674
amker <me@marekcerny.com>
2020-03-29 16:14:32 +0200

    fix cuda

--- cifar10/utils.py
+++ cifar10/utils.py
@@ -79,11 +79,13 @@ def get_subsample_dataset_label_noise(trainset, subset, noise_size):
         trainsubset.targets[idx] = shuffle_targets_subset[idx - train_size + noise_size]
     return trainsubset
 
-def save_feature_space(model, dataloader, path):
+def save_feature_space(model, dataloader, path, cuda=True):
     import csv
     with path.open('w') as f:
         writer = csv.writer(f)
         for batch_idx, (inputs, targets) in enumerate(dataloader):
+            if cuda:
+                inputs, targets = inputs.cuda(), targets.cuda()
             feat_batch = model.to_feature_space(inputs)
             for clss, feat in zip(targets, feat_batch):
                 writer.writerow([clss.tolist()] + feat.tolist())
